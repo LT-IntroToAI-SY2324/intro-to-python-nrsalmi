a8bc69ac0b73276002f862c9adcc481f57256f47
Nathan Salmi <nrsalmi@cps.edu>
2023-09-08 10:29:28 -0500

    Rock Paper Scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,1 +1,35 @@
 # We will write a rock paper scissors game in class - Complete it in this file
+import random
+
+def get_user_choice():
+    choice = input("Enter your choice (rock/paper/scissors): ").lower()
+    while choice not in ["rock", "paper", "scissors"]:
+        print("Invalid choice. Please choose rock, paper, or scissors.")
+        choice = input("Enter your choice (rock/paper/scissors): ").lower()
+    return choice
+
+def get_computer_choice():
+    choices = ["rock", "paper", "scissors"]
+    return random.choice(choices)
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (
+        (user_choice == "rock" and computer_choice == "scissors") or
+        (user_choice == "paper" and computer_choice == "rock") or
+        (user_choice == "scissors" and computer_choice == "paper")
+    ):
+        return "You win!"
+    else:
+        return "Computer wins!"
+
+def play_game():
+    print("Welcome to Rock Paper Scissors!")
+    user_choice = get_user_choice()
+    computer_choice = get_computer_choice()
+    print(f"You chose {user_choice}, and the computer chose {computer_choice}.")
+    result = determine_winner(user_choice, computer_choice)
+    print(result)
+
+play_game()
